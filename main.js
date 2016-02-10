e721facf690fe7c8616214f3b78c266ff7353cbb
Will Carter <will.carter@me.com>
2016-02-10 13:32:01 +0000

    Optional allow template to be placed with 'parentView' set   pass Template.instance().view to show function after options

--- main.js
+++ main.js
@@ -1,51 +1,55 @@
-var $soloModal = null // Used when allowMultiple = false.
+var $soloModal = null; // Used when allowMultiple = false.
 
 // The public API.
 Modal = {
-	
-	allowMultiple: false,
-	
-	show: function(templateName, data, options){
-		
-		if($soloModal == null || this.allowMultiple){
-			
-			var parentNode = document.body
-			
-			var view = Blaze.renderWithData(Template[templateName], data, parentNode)
-			
-			var domRange = view._domrange // TODO: Don't violate against the public API.
-			
-			var $modal = domRange.$('.modal')
-			
-			$modal.on('shown.bs.modal', function(event){
-				$modal.find('[autofocus]').focus()
-			})
-			
-			$modal.on('hidden.bs.modal', function(event){
-				Blaze.remove(view)
-				$soloModal = null
-			})
-			
-			$soloModal = $modal
-			
-			$modal.modal(options ? options : {})
-			
-		}
-		
-	},
-	
-	hide: function(/* optional */ template){
-		
-		if(template instanceof Blaze.TemplateInstance){
-			
-			template.$('.modal').modal('hide')
-			
-		}else if($soloModal != null){
-			
-			$soloModal.modal('hide')
-			
-		}
-		
-	}
-	
-}
+
+  allowMultiple: false,
+
+  show: function (templateName, data, options, parentView) {
+
+    if ($soloModal === null || this.allowMultiple) {
+
+      // var parentNode = placeholder ? document.getElementById(placeholder) : document.body;
+      var parentNode = document.body;
+
+
+      var view = Blaze.renderWithData(Template[templateName], data, parentNode, null, parentView);
+
+      var domRange = view._domrange; // TODO: Don't violate against the public API.
+
+      var $modal = domRange.$('.modal');
+
+      $modal.on('shown.bs.modal', function (event) {
+        $modal.find('[autofocus]')
+          .focus();
+      });
+
+      $modal.on('hidden.bs.modal', function (event) {
+        Blaze.remove(view);
+        $soloModal = null;
+      });
+
+      $soloModal = $modal;
+
+      $modal.modal(options ? options : {});
+
+    }
+
+  },
+
+  hide: function ( /* optional */ template) {
+
+    if (template instanceof Blaze.TemplateInstance) {
+
+      template.$('.modal')
+        .modal('hide');
+
+    } else if ($soloModal !== null) {
+
+      $soloModal.modal('hide');
+
+    }
+
+  }
+
+};
